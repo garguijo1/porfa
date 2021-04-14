60cac612cdc16b0381f5f232e0901e7c9cbb517e
gabriel <gguillenmelgarejo@gmail.com>
2021-04-13 22:00:39 -0500

    hola

---
+++ Assets/scripts/CamaraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+    public GameObject personaje;
+    public Vector2 minCamPos, maxCamPos;
+    public float suavizado;
+    private Vector2 velocidad;
+
+   // private Vector3 posicion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //posicion = transform.position - personaje.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float posx = Mathf.SmoothDamp(transform.position.x,
+            personaje.transform.position.x,ref velocidad.x, suavizado);
+        float posy = Mathf.SmoothDamp(transform.position.y,
+            personaje.transform.position.y, ref velocidad.y, suavizado);
+        //float posx = personaje.transform.position.x;
+        //float posy = personaje.transform.position.y;
+
+        transform.position = new Vector3(Mathf.Clamp(posx,minCamPos.x,maxCamPos.x),
+                                         Mathf.Clamp(posy,minCamPos.y,maxCamPos.y), 
+                                        transform.position.z);
+
+        //transform.position = personaje.transform.position + posicion;
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Text conteo;
+   // public Text win;
+    private int punto;
+    private Vector3 posicion;
+
+
+    private bool salto = true;
+    private bool inicio = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        punto = 0;
+        SetCountText();
+       // win.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-250f * Time.deltaTime, 0));
+
+        }
+
+        if (Input.GetKey("right"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(250f * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKeyDown("up") && salto )
+        {
+
+            salto = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 300f));
+
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+         if(collision.transform.tag == "piso")
+        {
+            salto = true;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("moneda"))
+        {
+            collision.gameObject.SetActive(false);
+            punto = punto + 1;
+            SetCountText();
+        }
+
+       if (collision.gameObject.CompareTag("agua"))
+        {
+            salto = false;
+        }
+
+        if (collision.gameObject.CompareTag("daño"))
+        {
+            salto = false;
+            gameObject.GetComponent<Transform>().transform.position = posicion;
+
+        }
+
+        if (collision.gameObject.CompareTag("spawn") && inicio)
+        {
+            posicion = collision.gameObject.GetComponent<Transform>().transform.position; 
+
+        }
+
+        if (collision.gameObject.CompareTag("check"))
+        {
+            inicio = false;
+            posicion = collision.gameObject.GetComponent<Transform>().transform.position;
+
+        }
+    }
+
+    void SetCountText()
+    {
+      // conteo.text = "Puntos: " + punto.ToString();
+        if(punto >= 5)
+        {
+            //win.text = "GANASTE";
+        }
+    }
+
+}
