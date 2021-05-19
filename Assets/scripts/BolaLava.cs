e5ab9937805d0e8665476babe1691d59f4cbb0b3
gabriel <gguillenmelgarejo@gmail.com>
2021-05-19 01:59:06 -0500

    bola de fuego

---
+++ Assets/scripts/BolaLava.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaLava : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    public float speed;
+    public float altura;
+    public Transform p;
+
+    private bool arriba;
+    private bool abajo;
+
+    void Start()
+    {
+        arriba = true;
+        abajo = true;
+       rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y >= (p.position.y + altura) && arriba)
+        {
+
+            transform.localScale = new Vector2(1, -1);
+            //rig.velocity = transform.up * -speed * Time.deltaTime;
+            speed *= -1;
+            arriba = false;
+            abajo = true;
+        }
+         
+        if(transform.position.y <= p.position.y && abajo)
+        {
+            transform.localScale = new Vector2(1, 1);
+            //rig.velocity = transform.up * speed * Time.deltaTime;
+            speed *= -1;
+            arriba = true;
+            abajo = false;
+        }
+
+        rig.velocity = transform.up * speed * Time.deltaTime;
+
+
+
+        
+
+
+    }
+
+}
